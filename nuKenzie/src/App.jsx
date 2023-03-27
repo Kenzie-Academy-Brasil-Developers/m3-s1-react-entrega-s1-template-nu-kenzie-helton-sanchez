9524ac7cc00cc13dfacc316babfeba2a3dceb791
Helton Sanchez <helton.sanchez@ymail.com>
2023-03-27 03:59:26 -0300

    feat: m3 s1 activity Nu Kenzie css responsive

--- nuKenzie/src/App.jsx
+++ nuKenzie/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import { IndexHeader } from './components/Header/index'
 import { Count } from './components/Main/Count/count.jsx'
 import { Form } from './components/Main/Form/form.jsx'
-import { Card } from './components/Main/SectionList/card'
 import { FinancialSummaryList } from './components/Main/SectionList/list.jsx'
+
+import { v4 as uuidv4} from 'uuid';
+
+
 
 import './styles/reset.css'
 import './styles/globalStyle.css'
@@ -11,13 +14,21 @@ import './styles/home.css'
 
 function App() {
 
+  const [summary, setSummary] = useState([])
+
+  function addToSummary (formData) {
+    const newCard = { ...formData, id: uuidv4()}
+    const newCardList = [...summary, newCard];
+    setSummary(newCardList);
+  }
+
   return (
     <>
 
     <IndexHeader />
     <main>
       <section className='container__form'>
-        <Form />
+        <Form addToSummary={addToSummary} />
         <Count />
       </section>
       <section className='container__summary'>
--- nuKenzie/src/components/Main/Form/form.jsx
+++ nuKenzie/src/components/Main/Form/form.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react"
 
-export function Form () {
+export function Form ({ addToSummary }) {
+    const [description, setDescription] = useState("");
+    const [value, setValue] = useState("");
+    const [typeValue, setTypeValue] = useState("");
+
+    const submit = (event) => {
+        event.preventDefault();
+
+        const formData = {description, value};
+        addToSummary(formData);
+    }
 
     return(
-        <form>
+
+        <form onSubmit={submit}>
 
             <label htmlFor="description">Descrição</label>
-            <input type={"text"} name="description" placeholder="Digite aqui sua descrição"/>
+            <input type="text" name="description" placeholder="Digite aqui sua descrição" value={description} onChange={(event) => setDescription(event.target.value)}/>
             <span>Ex: Compra de roupas.</span>
+
             <label htmlFor="value">Valor (R$)</label>
-            <input type={"number"} name="value" />
+            <input type="number" name="value" value={value} onChange={(event) => setValue(event.target.value)}/>
+
             <label htmlFor="typeValue">Tipo de Valor</label>
-            <select name="typeValue" >
-                <option value="">Entrada</option>
+            <select name="typeValue" value={typeValue} onChange={(event) => setTypeValue(event.target.value)}>
+                <option value="entrada">Entrada</option>
+                <option value="saida">Saida</option>
             </select>
             <button type="submit">Inserir Valor</button>
 
